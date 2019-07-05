917d447b5645034d888ab57caa9530444a65c3b4
ZetaChow <zetachow@163.com>
2019-07-05 16:32:39 +0800

    测试pin

--- codes/myraspi/main.go
+++ codes/myraspi/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"gobot.io/x/gobot"
@@ -12,30 +11,42 @@ import (
 
 func main() {
 	r := raspi.NewAdaptor()
-
-	leds := []*gpio.LedDriver{}
-
-	for i := 1; i <= 40; i++ {
-		leds = append(leds, gpio.NewLedDriver(r, fmt.Sprintf("%d", i)))
-
-	}
+	ledC := gpio.NewLedDriver(r, "8")
+	ledR := gpio.NewLedDriver(r, "10")
 
 	work := func() {
+		gobot.Every(2*time.Second, func() {
 
-		gobot.Every(10*time.Second, func() {
-			for _, led := range leds {
-				log.Println("led :", led.Name(), led.Pin(), led.State())
-				if err := led.Toggle(); err != nil {
-					log.Println(led.State())
+			go func() {
+				for {
+
+					if ledR.State() {
+						fmt.Println("receiveing")
+					}
 				}
+			}()
+
+			fmt.Println("send start")
+			err := ledC.On()
+			fmt.Println("ledc on", ledC.State(), ledR.State())
+			if err != nil {
+				fmt.Println(err.Error())
 			}
+			time.Sleep(10 * time.Millisecond)
+			err = ledC.Off()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Println("send over")
+
+			fmt.Println("ledc off", ledC.State(), ledR.State())
+
 		})
 	}
 
 	robot := gobot.NewRobot("blinkBot",
 		[]gobot.Connection{r},
-		//[]gobot.Device{leds},
-		leds,
+		[]gobot.Device{ledC, ledR},
 		work,
 	)
 
